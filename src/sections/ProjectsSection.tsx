a2e7b13c4650f126a3f3af9d8fd23dc78e12be69
yoom <fdgh134@naver.com>
2025-04-15 00:23:49 +0900

    프로젝트 섹션 배열로 나열

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -27,39 +27,35 @@ export default function ProjectsSection() {
     );
   }, []);
 
+  const projects = [
+    { title: "프로젝트 1", desc: "설명 1" },
+    { title: "프로젝트 2", desc: "설명 2" },
+    { title: "프로젝트 3", desc: "설명 3" },
+    { title: "프로젝트 4", desc: "설명 4" },
+    { title: "프로젝트 5", desc: "설명 5" },
+    { title: "프로젝트 6", desc: "설명 6" },
+    { title: "프로젝트 7", desc: "설명 7" },
+    { title: "프로젝트 8", desc: "설명 8" },
+  ];
+
   return (
     <section
       id="projects"
       ref={sectionRef}
-      className="h-screen scroll-mt-16 bg-white dark:bg-zinc-900 px-[5vw] py-16 flex flex-col items-center "
+      className="h-screen scroll-mt-16 bg-white dark:bg-zinc-900 px-[5vw] py-20 flex flex-col items-center "
     >
-      <h2 className="text-3xl font-bold mb-10">🛠️ 프로젝트</h2>
+      <h2 className="text-5xl font-bold mb-20">🛠️ 프로젝트</h2>
       <div className="overflow-x-auto">
         <div ref={titleRef} className="grid grid-cols-4 gap-[2vw] mt-10">
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 1
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 2
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 3
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 4
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 5
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 6
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 7
-          </div>
-          <div className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded shadow p-4">
-            프로젝트 8
-          </div>
+          {projects.map((project, i) => (
+            <div
+              key={i}
+              className="w-[400px] h-[300px] bg-zinc-100 dark:bg-zinc-700 rounded-lg shadow p-4"
+            >
+              <h3 className="text-xl font-semibold">{project.title}</h3>
+              <p className="text-gray-500 dark:text-white">{project.desc}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
