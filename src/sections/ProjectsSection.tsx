844ad174cb80de98bc03999370c61a71d658d48f
yoom <fdgh134@naver.com>
2025-04-16 00:02:36 +0900

    이미지 경로 수정

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -28,15 +28,15 @@ export default function ProjectsSection() {
   }, []);
 
   const projects = [
-    { title: "마비노기 Open Api 웹페이지", desc: "인게임 내 경매장, 전체 채팅, NPC 아이템 정보 확인 페이지", img: "/public/img/mabinogiApi.jpg", link: "https://soapy-ixora-d44.notion.site/Mabinogi-Api-1d64ddded97d8039a863f8abb0b58fb4" },
-    { title: "더리워드 App", desc: "더리워드 App UI 디자인", img:"/public/img/theReword.png", link: "https://soapy-ixora-d44.notion.site/APP-1d64ddded97d80c5a50acad3499a5dae?pvs=73" },
-    { title: "GLOX 브랜드 페이지", desc: "자전거 캐리어 제품 웹페이지", img: "/public/img/meta-glox.png", link: "https://soapy-ixora-d44.notion.site/GLOX-1d64ddded97d8057bf53cef6eb87a506" },
-    { title: "Metainnovation Robot", desc: "메타이노베이션 서빙로봇 웹페이지", img: "/public/img/meta-robot.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-Robot-1d64ddded97d80e2bce1d14d057a752c" },
-    { title: "Metainnovation Table Order", desc: "메타이노베이션 테이블오더 웹페이지", img: "/public/img/meta-tableorder.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-TableOrder-1d64ddded97d80888419fe8db58fce1c" },
-    { title: "Metainnovation POS", desc: "메타이노베이션 포스 웹페이지", img: "/public/img/meta-pos.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-POS-1d64ddded97d80a5a5ced4335a9435de?pvs=73" },
-    { title: "Metainnovation", desc: "메타이노베이션 웹페이지", img: "/public/img/meta-company.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-1d64ddded97d802abe19ed6fde8a3e62?pvs=73" },
-    { title: "UIUX 디자인", desc: "Figma로 작업한 미구현 디자인 작업물", img: "/public/img/uiux/nail-thum.png", link: "https://soapy-ixora-d44.notion.site/UIUX-1d64ddded97d80c3b85dff7f6cb2031b" },
-    { title: "상세페이지 작업", desc: "온라인 쇼핑몰에 등록된 상세페이지 작업물", img: "/public/img/nak10.png", link: "https://soapy-ixora-d44.notion.site/1d64ddded97d808b88d0ea4f744b38d5" },
+    { title: "마비노기 Open Api 웹페이지", desc: "인게임 내 경매장, 전체 채팅, NPC 아이템 정보 확인 페이지", img: "/img/mabinogiApi.jpg", link: "https://soapy-ixora-d44.notion.site/Mabinogi-Api-1d64ddded97d8039a863f8abb0b58fb4" },
+    { title: "더리워드 App", desc: "더리워드 App UI 디자인", img:"/img/theReword.png", link: "https://soapy-ixora-d44.notion.site/APP-1d64ddded97d80c5a50acad3499a5dae?pvs=73" },
+    { title: "GLOX 브랜드 페이지", desc: "자전거 캐리어 제품 웹페이지", img: "/img/meta-glox.png", link: "https://soapy-ixora-d44.notion.site/GLOX-1d64ddded97d8057bf53cef6eb87a506" },
+    { title: "Metainnovation Robot", desc: "메타이노베이션 서빙로봇 웹페이지", img: "/img/meta-robot.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-Robot-1d64ddded97d80e2bce1d14d057a752c" },
+    { title: "Metainnovation Table Order", desc: "메타이노베이션 테이블오더 웹페이지", img: "/img/meta-tableorder.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-TableOrder-1d64ddded97d80888419fe8db58fce1c" },
+    { title: "Metainnovation POS", desc: "메타이노베이션 포스 웹페이지", img: "/img/meta-pos.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-POS-1d64ddded97d80a5a5ced4335a9435de?pvs=73" },
+    { title: "Metainnovation", desc: "메타이노베이션 웹페이지", img: "/img/meta-company.png", link: "https://soapy-ixora-d44.notion.site/Metainnovation-1d64ddded97d802abe19ed6fde8a3e62?pvs=73" },
+    { title: "UIUX 디자인", desc: "Figma로 작업한 미구현 디자인 작업물", img: "/img/uiux/nail-thum.png", link: "https://soapy-ixora-d44.notion.site/UIUX-1d64ddded97d80c3b85dff7f6cb2031b" },
+    { title: "상세페이지 작업", desc: "온라인 쇼핑몰에 등록된 상세페이지 작업물", img: "/img/nak10.png", link: "https://soapy-ixora-d44.notion.site/1d64ddded97d808b88d0ea4f744b38d5" },
   ];
 
   return (
